0818314c69f4cb5c889f370f1464c0bd1e9bbd8b
Rafly Andrian W <raflyandrian@gmail.com>
2025-08-04 16:19:11 +0700

    initial commit

---
+++ src/main/java/com/blitzkrieg/apirequest/ApiRequestApplication.java
@@ -0,0 +1,13 @@
+package com.blitzkrieg.apirequest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRequestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiRequestApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/blitzkrieg/apirequest/controller/PostController.java
@@ -0,0 +1,4 @@
+package com.blitzkrieg.apirequest.controller;
+
+public class PostController {
+}
