1279a8918a6b8f57d3fd7e070233f7a6e5231872
Rafly Andrian W <raflyandrian@gmail.com>
2025-08-05 00:35:15 +0700

    feat(Post): add JPA annotations and lifecycle methods for entity management

--- src/main/java/com/blitzkrieg/apirequest/models/Post.java
+++ src/main/java/com/blitzkrieg/apirequest/models/Post.java
@@ -1,6 +1,9 @@
 package com.blitzkrieg.apirequest.models;
 
+
 import com.fasterxml.jackson.annotation.JsonFormat;
+import de.huxhorn.sulky.ulid.ULID;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,14 +11,32 @@ import java.util.Date;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "posts")
 public class Post {
+    @Id
     private String id;
     private String title;
     private String content;
     private String author;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private Date createdAt;
+    private Date modifiedAt;
+
+    @PrePersist
+    public void ensureId() {
+        if (this.id == null) {
+            this.id = new ULID().nextULID();
+        }
+        if(this.createdAt == null) {
+            this.createdAt = new Date();
+        }
+    }
+    @PreUpdate
+    public void updateModifiedAt() {
+        this.modifiedAt = new Date();
+    }
 
     public Post() {
+        // Default constructor for JPA
     }
 }
